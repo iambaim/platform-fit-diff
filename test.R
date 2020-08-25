a3c6d675f9fbbbace8b14bef46639967169f3eeb
Ibrahim Umar <ibrahim.umar@hi.no>
2020-08-25 10:29:45 +0200

    first commit

---
+++ test.R
@@ -0,0 +1,39 @@
+library(FLCore)
+library(FLBRP)
+
+# Predict recruitment
+predict_recruitment <- function(stk, model) {
+
+  # Create the model
+  newFL <- fmle(as.FLSR(stk, model=model))
+
+  # Get the latest ssb
+  ssb <- tail(ssb(stk), 1)
+
+  # Predict
+  prm <- params(newFL)
+
+  s <- a <- prm[[1]] # Extract 'a' parameter
+
+  if(length(prm) > 1)
+    R0 <- b <- params(newFL)[[2]] # Extract 'b' parameter
+
+  if(length(prm) > 2)
+    v <- c <- params(newFL)[[3]] # Extract 'c' parameter
+
+  # Use the model formula in FLCore
+  rec <- eval(parse(text = as.character(model()$model[3])))
+
+  return(list(srmodel = newFL, rec = as.numeric(rec)))
+}
+
+stk1 <- readRDS("stk.2002.linux.rds")
+stk2 <- readRDS("stk.2002.macos.rds")
+
+pr1 <- predict_recruitment(stk1, shepherd)
+pr2 <- predict_recruitment(stk2, shepherd)
+
+print(pr1$rec)
+print(pr2$rec)
+
+all.equal(stk1, stk2, tolerance=0)
